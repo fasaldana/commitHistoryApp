d32567a4ca2e9ad9c4793d5bb1071645ef709879
fasaldana <fernando_s_98@hotmail.com>
2023-10-04 16:17:54 -0500

    Added new Components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Nabvar from "./components/Nabvar";
+import GitHistory from "./components/gitHistory";
 
 function App() {
   return (
     <div className="app">
-
+      <Nabvar />
+      <div className="container">
+        <GitHistory />
+        <GitHistory />
+      </div>
     </div>
   );
 }
