375bd25f14c9154fa11fd18171c78ec93984c9f2
fasaldana <fernando_s_98@hotmail.com>
2023-10-04 16:53:42 -0500

    Update styling for table and background

--- src/components/gitHistory.jsx
+++ src/components/gitHistory.jsx
@@ -17,28 +17,33 @@ const GitHistory = (props) => {
   }, [repoName]);
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Commit</th>
-          <th>Author</th>
-          <th>Date</th>
-          <th>Message</th>
-        </tr>
-      </thead>
-      <tbody>
-        {exampleData.map((data) => {
-          return (
-            <tr key={data.sha}>
-              <td>{data.sha}</td>
-              <td>{data.author}</td>
-              <td>{data.date}</td>
-              <td>{data.message}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="tab-container">
+      <div className="title">
+        <h3>Commit History for {repoName}</h3>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>SHA</th>
+            <th>Author</th>
+            <th>Date</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exampleData.map((data) => {
+            return (
+              <tr key={data.sha}>
+                <td>{data.sha}</td>
+                <td>{data.author}</td>
+                <td>{data.date}</td>
+                <td>{data.message}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
